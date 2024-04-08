6ef1a5fc653479341720109b92eed5eab9b1410f
sm0bin <shehjad0mobin@gmail.com>
2024-04-09 04:27:05 +0600

    authVerify and authController

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,14 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const verifyToken = require('../middlewares/verifyToken');
 
-router.post('/auth/signup', userController.signupUser);
-router.post('/auth/login', userController.loginUser);
-router.post('/auth/forgot-password', userController.forgotPassword);
-router.post('/auth/reset-password/:token', userController.resetPassword);
 router.get('/:email', verifyToken, userController.getUserByEmail);
 router.post('/create-jwt', userController.createUserJWT);
 router.put('/:email', verifyToken, userController.updateUser);
 router.delete('/:email', verifyToken, userController.deleteUser);
-// Define routes for update and delete operations
 
 module.exports = router;
