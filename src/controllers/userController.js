ebbda1e430d7d1195cfda46326adf0b28159a1ad
sm0bin <shehjad0mobin@gmail.com>
2024-04-30 16:09:41 +0600

    search func

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,10 +3,38 @@ const jwt = require('jsonwebtoken');
 
 exports.getAllUsers = async (req, res) => {
     try {
-        const users = await User.find();
+        const searchQuery = req.query.search;
+        const regex = new RegExp(searchQuery, 'i');
+
+        const query = {
+            $or: [
+                { email: regex },
+                { 'profile.basic.name': regex },
+                { 'profile.basic.role': regex },
+                { 'profile.education.university': regex },
+                { 'profile.education.department': regex },
+                { 'profile.education.session': regex },
+                { 'profile.contact.address': regex },
+            ]
+        };
+
+        const options = {
+            sort: {
+                'profile.education.session': 1
+            }
+        };
+
+        let users;
+        if (searchQuery) {
+            users = await User.find(query, {}, options);
+        } else {
+            users = await User.find({}, {}, options);
+        }
+
         res.status(200).json({ status: true, users });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
+
 
